21d2b3c0d4dfe694041bc380cbc3811b3c2249a3
t3min4l <hainam.nguyen.ict@gmail.com>
2018-11-10 23:47:25 +0700

    update

--- constants.py
+++ constants.py
@@ -4,5 +4,18 @@ import sys
 PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))
 DATA_DIR = os.path.join(PROJECT_DIR, 'datas')
 SAVED_MODEL_DIR = os.path.join(PROJECT_DIR, 'saved_model')
-KYLBERG_MODEL = os.path.join(SAVED_MODEL_DIR, 'kylberg.bin')
-KTH_2_MODEl = os.path.join(SAVED_MODEL_DIR, 'kth_2.bin')
+
+KYLBERG_SVM_MODEL = os.path.join(SAVED_MODEL_DIR, 'kylberg_svm.bin')
+KTH_TIPS_2_SVM_MODEL = os.path.join(SAVED_MODEL_DIR, 'kth_2_tips_svm.bin')
+
+KYlBERG_KNN_MODEL = os.path.join(SAVED_MODEL_DIR, 'kylberg_knn.bin')
+KTH_TIPS_2_KNN_MODEL = os.path.join(SAVED_MODEL_DIR, 'kth_2_tips_knn.bin')
+
+KYLBERG_NB_MODEL = os.path.join(SAVED_MODEL_DIR, 'kylberg_nb.bin')
+KTH_TIPS_2_NB_MODEL = os.path.join(SAVED_MODEL_DIR, 'kth_2_tips_nb.bin')
+
+
+KNN_N_NEIGHBORS = 5
+
+KYLBERG_CLASS_NUM = 28
+KTH_TIPS_2_CLASS_NUM = 44
--- main.py
+++ main.py
@@ -26,39 +26,64 @@ y_train_kylberg = None
 x_test_kylberg = None
 y_test_kylberg = None
 
-x_train_kth_2 = None
-y_train_kth_2 = None
-x_test_kth_2 = None
-y_test_kth_2 =None
+x_train_kth_tips_2 = None
+y_train_kth_tips_2 = None
+x_test_kth_tips_2 = None
+y_test_kth_tips_2 =None
+
+
 
 
 if args.train:
     if args.model == 'svm':
         if args.kernel_svm == 'linear':
             clf_kylberg = SVC(kernel=args.kernel, C=args.C)
-            clf_kth_2 = SVC(kernel=args.kernel, C=args.C)
+            clf_kth_tips_2 = SVC(kernel=args.kernel, C=args.C)
 
             clf_kylberg.fit(x_train_kylberg, y_train_kylberg)
-            with open(constants.KYLBERG_MODEL, 'wb') as kylberg_model_bin:
+            with open(constants.KYLBERG_SVM_MODEL, 'wb') as kylberg_model_bin:
                 pickle.dump(clf_kylberg, kylberg_model_bin)
 
-            clf_kth_2.fit(x_train_kth_2, y_train_kth_2)
-            with open(constants.KTH_2_MODEl, 'wb') as kth_2_model_bin:
-                pickle.dump(clf_kth_2, kth_2_model_bin)
+            clf_kth_tips_2.fit(x_train_kth_tips_2, y_train_kth_tips_2)
+            with open(constants.KTH_TIPS_2_SVM_MODEL, 'wb') as kth_tips_2_model_bin:
+                pickle.dump(clf_kth_tips_2, kth_tips_2_model_bin)
 
 
         else:
             param_grid = {'C': [0.1, 1, 10, 100], 'gamma': [1, 0.1, 0.01, 0.001, 0.00001, 10]}
             clf_kylberg_grid = GridSearchCV(SVC(), param_grid, verbose=1)
-            clf_kth_2_grid = GridSearchCV(SVC(), param_grid, verbose=1)
+            clf_kth_tips_2_grid = GridSearchCV(SVC(), param_grid, verbose=1)
 
             clf_kylberg_grid.fit(x_train_kylberg, y_train_kylberg)
-            with open(constants.KYLBERG_MODEL, 'wb') as kylberg_model_bin:
+            with open(constants.KYLBERG_SVM_MODEL, 'wb') as kylberg_model_bin:
                 pickle.dump(clf_kylberg_grid, kylberg_model_bin)
 
-            clf_kth_2_grid.fit(x_train_kth_2, y_train_kth_2)
-            with open(constants.KTH_2_MODEl, 'wb') as kth_2_model_bin:
-                pickle.dump(clf_kth_2_grid, kth_2_model_bin)
+            clf_kth_tips_2_grid.fit(x_train_kth_tips_2, y_train_kth_tips_2)
+            with open(constants.KTH_TIPS_2_SVM_MODEL, 'wb') as kth_tips_2_model_bin:
+                pickle.dump(clf_kth_tips_2_grid, kth_tips_2_model_bin)
+    if args.model == 'knn':
+        clf_kylberg = KNeighborsClassifier(constants.KYLBERG_CLASS_NUM*constants.KNN_N_NEIGHBORS, weights='distance')
+        clf_kth_tips_2 = KNeighborsClassifier(constants.KTH_TIPS_2_CLASS_NUM*constants.KNN_N_NEIGHBORS, weights='distance')
+
+        clf_kylberg.fit(x_train_kylberg, y_train_kylberg)
+        with open(constants.KYlBERG_KNN_MODEL, 'wb') as kylberg_model_bin:
+            pickle.dump(clf_kylberg, kylberg_model_bin)
+
+        clf_kth_tips_2.fit(x_train_kth_tips_2, y_train_kth_tips_2)
+        with open(constants.KTH_TIPS_2_KNN_MODEL, 'wb') as kth_tips_2_model_bin:
+            pickle.dump(clf_kth_tips_2, kth_tips_2_model_bin)
+
+    if args.model == 'nb':
+        clf_kylberg = MultinomialNB(alpha=1, fit_prior=False, class_prior=False)
+        clf_kth_tips_2 = MultinomialNB(alpha=1, fit_prior=False, class_prior=False)
+
+        clf_kylberg.fit(x_train_kylberg, y_train_kylberg)
+        with open(constants.KYLBERG_NB_MODEL, 'wb') as kylberg_model_bin:
+            pickle.dump(clf_kylberg, kylberg_model_bin)
+
+        clf_kth_tips_2.fit(x_train_kth_tips_2, y_train_kth_tips_2)
+        with open(constants.KTH_TIPS_2_NB_MODEL, 'wb') as kth_tips_2_model_bin:
+            pickle.dump(clf_kth_tips_2, kth_tips_2_model_bin)
 
 if args.predict:
 
