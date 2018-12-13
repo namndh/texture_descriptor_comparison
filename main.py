b3fdbb110ef205156ca2389f73513be04850f9f2
t3min4l <hainam.nguyen.ict@gmail.com>
2018-12-13 23:37:33 +0700

    add result

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ import pickle
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split, GridSearchCV
-# from torch.utils.data import Dataset, DataLoader
-# import cv2
+from torch.utils.data import Dataset, DataLoader
+import cv2
 import csv
 import datetime
-# from subprocess import call
+from subprocess import call
 
 import constants
 from utils import *
@@ -25,94 +25,97 @@ args = parser.parse_args()
 kth_config = configs('kth')
 kylberg_config = configs('kylberg')
 models = ['svm', 'nb', 'knn']
-svm_kernels = ['poly','rbf']
-descriptors = ['db4',  'lbp', 'glcm']
-datasets = ['kylberg', 'kth']
+svm_kernels = ['linear']
+descriptors = ['glcm']
+datasets = ['kylberg']
 
 
-# if args.process_data:
-# 	kylberg_folders = [x[0] for x in os.walk(constants.KYLBERG_DATA_PATH)]
-# 	kylberg_labels = get_labels(kylberg_folders, constants.KYLBERG_FOLDER)
-# 	kylberg_train_paths, kylberg_test_paths = build_fns_labels(kylberg_labels, 'kylberg')
+if args.process_data:
+	kylberg_folders = [x[0] for x in os.walk(constants.KYLBERG_DATA_PATH)]
+	kylberg_labels = get_labels(kylberg_folders, constants.KYLBERG_FOLDER)
+	kylberg_train_paths, kylberg_test_paths = build_fns_labels(kylberg_labels, 'kylberg')
 
-# 	kth_folders = [x[0] for x in os.walk(constants.KTH_TIPS2_DATA_PATH)]
-# 	kth_labels = get_labels(kth_folders, constants.KTH_TIPS2_FOLDER)
-# 	kth_train_paths, kth_test_paths = build_fns_labels(kth_labels, 'kth')
+	kth_folders = [x[0] for x in os.walk(constants.KTH_TIPS2_DATA_PATH)]
+	kth_labels = get_labels(kth_folders, constants.KTH_TIPS2_FOLDER)
+	kth_train_paths, kth_test_paths = build_fns_labels(kth_labels, 'kth')
 
-# 	for descriptor in descriptors:
-# 		kylberg_train_data = data_loader(kylberg_train_paths, transform=extractors[descriptor])
-# 		with open(constants.datas_paths[descriptor]['kylberg_train'], 'wb') as f:
-# 			pickle.dump(kylberg_train_data, f)
-# 		kylberg_test_data = data_loader(kylberg_test_paths, transform=extractors[descriptor])
-# 		with open(constants.datas_paths[descriptor]['kylberg_test'], 'wb') as f:
-# 			pickle.dump(kylberg_test_data, f)
+	for descriptor in descriptors:
+		kylberg_train_data = data_loader(kylberg_train_paths, transform=extractors[descriptor])
+		with open(constants.datas_paths[descriptor]['kylberg_train'], 'wb') as f:
+			pickle.dump(kylberg_train_data, f)
+		kylberg_test_data = data_loader(kylberg_test_paths, transform=extractors[descriptor])
+		with open(constants.datas_paths[descriptor]['kylberg_test'], 'wb') as f:
+			pickle.dump(kylberg_test_data, f)
 
-# 		kth_train_data = data_loader(kth_train_paths, transform=extractors[descriptor])
-# 		with open(constants.datas_paths[descriptor]['kth_train'], 'wb') as f:
-# 			pickle.dump(kth_train_data, f)
-# 		kth_test_data = data_loader(kth_test_paths, transform=extractors[descriptor])
-# 		with open(constants.datas_paths[descriptor]['kth_test'], 'wb') as f:
-# 			pickle.dump(kth_test_data, f)
+		kth_train_data = data_loader(kth_train_paths, transform=extractors[descriptor])
+		with open(constants.datas_paths[descriptor]['kth_train'], 'wb') as f:
+			pickle.dump(kth_train_data, f)
+		kth_test_data = data_loader(kth_test_paths, transform=extractors[descriptor])
+		with open(constants.datas_paths[descriptor]['kth_test'], 'wb') as f:
+			pickle.dump(kth_test_data, f)
 
-# 	print('Finished processing and saving data!')
+	print('Finished processing and saving data!')
 
-# if args.evaluate:
-# 	x = datetime.datetime.now()
-# 	time = x.strftime("%H:%M-%d-%b-%Y")
-# 	time_created = time
-# 	f=open('./log_' + time +'_.txt', 'w+')
-# 	f.write('dataset, descriptor, classifier, acc\n')
-# 	f.close()
-# 	for descriptor in descriptors:
-# 		for dataset in datasets:
-# 			with open(constants.datas_paths[descriptor][dataset+'_train'], 'rb') as f:
-# 				train_data = pickle.load(f)
-# 			with open(constants.datas_paths[descriptor][dataset+'_test'], 'rb') as f:
-# 				test_data = pickle.load(f)
-
-# 			x_train, y_train = zip(*train_data)
-# 			x_train = list(x_train)
-# 			y_train = list(y_train)
-
-# 			x_test, y_test = zip(*test_data)
-# 			x_test = list(x_test)
-# 			y_test = list(y_test)
-
-# 			config = configs(dataset)
-# 			for model in models:
-# 				model_args = {'model':model, 'kernels_svm':svm_kernels}
-# 				clf = classifier(model_args, config)
-# 				clf.fit(x_train, y_train)
-# 				acc = clf.evaluate(x_test, y_test)
-# 				strings = str(dataset) + ',' + str(descriptor) + ',' + str(model) + ',' + str(acc) + '\n'
-# 				print(strings)
-# 				f=open('log_18:57-27-Nov-2018_.txt', 'a')
-# 				f.write(str(dataset) + ',' + str(descriptor) + ',' + str(model) + ',' + str(acc) + '\n')
-# 				f.close()
-
-dataset = 'kth'
-descriptor = 'db4'
-with open(constants.datas_paths[descriptor][dataset + '_train'], 'rb') as f:
-	train_data = pickle.load(f)
-
-with open(constants.datas_paths[descriptor][dataset + '_test'], 'rb') as f:
-	test_data = pickle.load(f)
-
-x_train, y_train = zip(*train_data)
-x_train = list(x_train)
-y_train = list(y_train)
-
-x_test, y_test = zip(*test_data)
-x_test = list(x_test)
-y_test = list(y_test)
-config = configs(dataset)
-for model in models:
-	model_args = {'model':model, 'kernels_svm':svm_kernels}
-	clf = classifier(model_args, config)
-	clf.fit(x_train, y_train)
-	acc = clf.evaluate(x_test, y_test)
-	strings = str(dataset) + ',' + str(descriptor) + ',' + str(model) + ',' + str(acc) + '\n'
-	print(strings)
-	f=open('./log_18:57-27-Nov-2018_.txt', 'a')
-	f.write(str(dataset) + ',' + str(descriptor) + ',' + str(model) + ',' + str(acc) + '\n')
+if args.evaluate:
+	x = datetime.datetime.now()
+	time = x.strftime("%H:%M-%d-%b-%Y")
+	time_created = time
+	log_path = './log_' + time +'_.txt'
+	f=open(log_path, 'w+')
+	f.write('dataset, descriptor, classifier, acc\n')
 	f.close()
+	for descriptor in descriptors:
+		for dataset in datasets:
+			with open(constants.datas_paths[descriptor][dataset+'_train'], 'rb') as f:
+				train_data = pickle.load(f)
+			with open(constants.datas_paths[descriptor][dataset+'_test'], 'rb') as f:
+				test_data = pickle.load(f)
+
+			x_train, y_train = zip(*train_data)
+			x_train = list(x_train)
+			y_train = list(y_train)
+
+			x_test, y_test = zip(*test_data)
+			x_test = list(x_test)
+			y_test = list(y_test)
+
+			# print(x_train[0].shape)
+			# print(x_test[0].shape)
+			config = configs(dataset)
+			for model in models:
+				model_args = {'model':model, 'kernels_svm':svm_kernels}
+				clf = classifier(model_args, config)
+				clf.fit(x_train, y_train)
+				acc = clf.evaluate(x_test, y_test)
+				strings = str(dataset) + ',' + str(descriptor) + ',' + str(model) + ',' + str(acc) + '\n'
+				print(strings)
+				f=open(log_path, 'a')
+				f.write(str(dataset) + ',' + str(descriptor) + ',' + str(model) + ',' + str(acc) + '\n')
+				f.close()
+
+# dataset = 'kth'
+# descriptor = 'db4'
+# with open(constants.datas_paths[descriptor][dataset + '_train'], 'rb') as f:
+# 	train_data = pickle.load(f)
+
+# with open(constants.datas_paths[descriptor][dataset + '_test'], 'rb') as f:
+# 	test_data = pickle.load(f)
+
+# x_train, y_train = zip(*train_data)
+# x_train = list(x_train)
+# y_train = list(y_train)
+
+# x_test, y_test = zip(*test_data)
+# x_test = list(x_test)
+# y_test = list(y_test)
+# config = configs(dataset)
+# for model in models:
+# 	model_args = {'model':model, 'kernels_svm':svm_kernels}
+# 	clf = classifier(model_args, config)
+# 	clf.fit(x_train, y_train)
+# 	acc = clf.evaluate(x_test, y_test)
+# 	strings = str(dataset) + ',' + str(descriptor) + ',' + str(model) + ',' + str(acc) + '\n'
+# 	print(strings)
+# 	f=open('./log_18:57-27-Nov-2018_.txt', 'a')
+# 	f.write(str(dataset) + ',' + str(descriptor) + ',' + str(model) + ',' + str(acc) + '\n')
+# 	f.close()
--- utils.py
+++ utils.py
@@ -6,13 +6,13 @@ from sklearn.naive_bayes import GaussianNB, MultinomialNB, BernoulliNB
 from sklearn.model_selection import GridSearchCV, cross_val_score
 from sklearn.metrics import accuracy_score
 import pickle
-# import cv2
+import cv2
 import numpy as np
-# import pywt/
-# from skimage import feature
-# from torch.utils.data import Dataset
+import pywt
+from skimage import feature
+from torch.utils.data import Dataset
 import glob
-# import mahotas as mt
+import mahotas as mt
 from random import shuffle
 
 import constants
@@ -32,7 +32,7 @@ class classifier():
 			self.clf = GridSearchCV(SVC(), self.param_grid, verbose=1, n_jobs=6)
 
 		if args['model'] == 'knn':
-			self.clf = KNeighborsClassifier(configs['class_num'], weights='distance', n_jobs=12)
+			self.clf = KNeighborsClassifier(5, weights='distance', n_jobs=12)
 		
 		if args['model'] == 'nb':
 			self.clf = GaussianNB()
@@ -54,133 +54,136 @@ class classifier():
 		with open(configs[path_key]) as model_bin:
 			pickle.dump(self.clf, model_bin)
 
-# def extract_mean_std(img):
-# 	mean, stdev = cv2.meanStdDev(img, mask=None)
-# 	mean = mean[0][0]
-# 	stdev = stdev[0][0]
-# 	return mean, stdev
+def extract_mean_std(img):
+	mean, stdev = cv2.meanStdDev(img, mask=None)
+	mean = mean[0][0]
+	stdev = stdev[0][0]
+	return mean, stdev
 
 
-# def gaborFilters_featuresExtractor(img, size=40):
-# 	kernels = list()
-# 	for i in np.arange(0, np.pi*2, np.pi/4):
-# 		for j in np.arange(2,size/8*1+2,1):
-# 			kernel=cv2.getGaborKernel((15, 15) , 3, i, j, 1, np.pi, cv2.CV_32F )
-# 			kernels.append(kernel)
-# 	features_vector = list()
-# 	for kernel in kernels:
-# 		img_output = cv2.filter2D(img, cv2.CV_8UC3, kernel)
-# 		features_vector.extend(extract_mean_std(img_output))
+def gaborFilters_featuresExtractor(img, size=40):
+	kernels = list()
+	for i in np.arange(0, np.pi*2, np.pi/4):
+		for j in np.arange(2,size/8*1+2,1):
+			kernel=cv2.getGaborKernel((15, 15) , 3, i, j, 1, np.pi, cv2.CV_32F )
+			kernels.append(kernel)
+	features_vector = list()
+	for kernel in kernels:
+		img_output = cv2.filter2D(img, cv2.CV_8UC3, kernel)
+		features_vector.extend(extract_mean_std(img_output))
 
-# 	return features_vector
+	return features_vector
 
-# def Haar_featuresExtractor(img, depth=3):
-# 	features_vector = list()
-# 	for i in range(depth):
-# 		coeff2 = pywt.dwt2(img, 'haar')
-# 		LL, (LH, HL, HH) = coeff2
-# 		features_vector.extend(extract_mean_std(LH))
-# 		features_vector.extend(extract_mean_std(HL))
-# 		features_vector.extend(extract_mean_std(HH))
-# 		img = LL
+def Haar_featuresExtractor(img, depth=3):
+	features_vector = list()
+	for i in range(depth):
+		coeff2 = pywt.dwt2(img, 'haar')
+		LL, (LH, HL, HH) = coeff2
+		features_vector.extend(extract_mean_std(LH))
+		features_vector.extend(extract_mean_std(HL))
+		features_vector.extend(extract_mean_std(HH))
+		img = LL
 
-# 	return features_vector
+	return features_vector
 
-# def DB4_featuresExtractor(img, depth=3):
-# 	features_vector = list()
-# 	for i in range(depth):
-# 		coeff2 = pywt.dwt2(img, 'db4')
-# 		LL, (LH, HL, HH) = coeff2
-# 		features_vector.extend(extract_mean_std(LH))
-# 		features_vector.extend(extract_mean_std(HL))
-# 		features_vector.extend(extract_mean_std(HH))
-# 		img = LL
+def DB4_featuresExtractor(img, depth=3):
+	features_vector = list()
+	for i in range(depth):
+		coeff2 = pywt.dwt2(img, 'db4')
+		LL, (LH, HL, HH) = coeff2
+		features_vector.extend(extract_mean_std(LH))
+		features_vector.extend(extract_mean_std(HL))
+		features_vector.extend(extract_mean_std(HH))
+		img = LL
 
-# 	return features_vector
+	return features_vector
 
-# def LBP_featuresExtractor(img, numPoints=24, radius=8, eps=1e-7):
-# 	lbp = feature.local_binary_pattern(img, numPoints, radius, method='uniform')
-# 	(hist, _) = np.histogram(lbp.ravel(), bins=np.arange(0, numPoints + 3), range=(0, numPoints + 2))
+def LBP_featuresExtractor(img, numPoints=24, radius=8, eps=1e-7):
+	lbp = feature.local_binary_pattern(img, numPoints, radius, method='uniform')
+	(hist, _) = np.histogram(lbp.ravel(), bins=np.arange(0, numPoints + 3), range=(0, numPoints + 2))
 
-# 	# normalize the histogram
-# 	hist = hist.astype("float")
-# 	hist /= (hist.sum() + eps)
+	# normalize the histogram
+	hist = hist.astype("float")
+	hist /= (hist.sum() + eps)
 
-# 	# return the histogram of Local Binary Patterns
-# 	return hist
+	# return the histogram of Local Binary Patterns
+	return hist
 
-# def GLCM_featuresExtractor(image):
-# 	textures = mt.features.haralick(image)
+def GLCM_featuresExtractor(image):
+	textures = mt.features.haralick(image)
 
-# 	# take the mean of it and return it
-# 	ht_mean = textures.mean(axis=0)
+	# take the mean of it and return it
+	ht_mean = textures.mean(axis=0)
 
-# 	return ht_mean
+	return ht_mean
 
 
-# class Mydataset(Dataset):
-# 	"""custom dataset to load and process image"""
-# 	def __init__(self, data, transform=None):
-# 		self.fns = list()
-# 		self.labels = list()
-# 		for fn, label in data:
-# 			self.fns.append(fn)
-# 			self.labels.append(label)
-# 		self.transform = transform
+class Mydataset(Dataset):
+	"""custom dataset to load and process image"""
+	def __init__(self, data, transform=None):
+		self.fns = list()
+		self.labels = list()
+		for fn, label in data:
+			self.fns.append(fn)
+			self.labels.append(label)
+		self.transform = transform
 
-# 	def __len__(self):
-# 		return len(self.fns)
+	def __len__(self):
+		return len(self.fns)
 
-# 	def __getitem__(self, idx):
-# 		image = cv2.imread(self.fns[idx])
-# 		gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-# 		gray_img = cv2.resize(gray_img, (256, 256))
-# 		if self.transform:
-# 			features_vector = self.transform(image)
-# 			return features_vector, self.labels[idx]
+	def __getitem__(self, idx):
+		image = cv2.imread(self.fns[idx])
+		gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+		gray_img = cv2.resize(gray_img, (256, 256))
+		if self.transform:
+			features_vector = self.transform(image)
+			return features_vector, self.labels[idx]
 		
 
-# def get_labels(folders, substrings):
-# 	substr1 = '/' + substrings
-# 	interval = len(substr1)+1
-# 	labels = list()
-# 	for folder in folders:
-# 		idx = folder.find(substr1)
-# 		label = folder[idx+interval:len(folder)]
-# 		labels.append(label)
-# 	labels = labels[1:]
-# 	return labels
+def get_labels(folders, substrings):
+	substr1 = '/' + substrings
+	interval = len(substr1)+1
+	labels = list()
+	for folder in folders:
+		idx = folder.find(substr1)
+		label = folder[idx+interval:len(folder)]
+		labels.append(label)
+	labels = labels[1:]
+	return labels
 
-# def build_fns_labels(labels, dataset, test_ratio=0.2):
-# 	train_paths = list()
-# 	test_paths = list()
-# 	train_ratio = 1 - test_ratio
-# 	for idx, label in enumerate(labels):
-# 		data_paths = list()
-# 		if dataset == 'kth':
-# 			label_dir = os.path.join(constants.KTH_TIPS2_DATA_PATH, label)
-# 		if dataset == 'kylberg':
-# 			label_dir = os.path.join(constants.KYLBERG_DATA_PATH, label)
+def build_fns_labels(labels, dataset, test_ratio=0.2):
+	train_paths = list()
+	test_paths = list()
+	train_ratio = 1 - test_ratio
+	for idx, label in enumerate(labels):
+		data_paths = list()
+		if dataset == 'kth':
+			label_dir = os.path.join(constants.KTH_TIPS2_DATA_PATH, label)
+		if dataset == 'kylberg':
+			label_dir = os.path.join(constants.KYLBERG_DATA_PATH, label)
 
-# 		fns = glob.glob(label_dir + '/*.png')
-# 		for fn in fns:
-# 			data_paths.append([fn, idx])
-# 		train_pivot = int(train_ratio*len(data_paths))
+		fns = glob.glob(label_dir + '/*.png')
+		for fn in fns:
+			data_paths.append([fn, idx])
+		train_pivot = int(train_ratio*len(data_paths))
 
-# 		for path in data_paths[:int(train_pivot)]:
-# 			train_paths.append(path)
-# 		for path in data_paths[int(train_pivot):]:
-# 			test_paths.append(path)
+		for path in data_paths[:int(train_pivot)]:
+			train_paths.append(path)
+		for path in data_paths[int(train_pivot):]:
+			test_paths.append(path)
 
-# 	return train_paths, test_paths
+	return train_paths, test_paths
 
 def data_loader(paths, transform):
 	dataset = list()
 	shuffle(paths)
 	for path, label in paths:
+		if not os.path.isfile(path):
+			print('{} is None'.format(path))
+			continue
 		img = cv2.imread(path)
 		img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-		img_gray = cv2.resize(img_gray, (256, 256))
+		img_gray = cv2.resize(img_gray, (128,  128))
 
 		data = transform(img_gray)
 		dataset.append([data, label])
